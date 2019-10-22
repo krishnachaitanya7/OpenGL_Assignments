af8bc96805bd0b8d576ba483d528773c4d477c78
krishna <chaitanyaradon89@gmail.com>
2019-10-22 13:03:21 -0600

    HW6 Starting

---
+++ homework_6/main.cpp
@@ -0,0 +1,47 @@
+#define GL_GLEXT_PROTOTYPES
+#include <GL/glut.h>
+#include "util.h"
+
+
+// Initializing Variables
+
+int utils::axes=0;
+int utils::mode=1;
+int utils::move=1;
+int utils::th=50;
+int utils::ph=0;
+int utils::fov=27;
+int utils::light=1;
+double utils::asp=1;
+double utils::dim=3.0;
+// Light values
+int utils::one       =   1;
+int utils::distance  =   2;
+int utils::inc       =  10;
+int utils::smooth    =   1;
+int utils::local     =   0;
+int utils::emission  =   0;
+int utils::ambient   =  30;
+int utils::diffuse   = 100;
+int utils::specular  =   0;
+int utils::shininess =   0;
+float utils::shiny   =   1;
+int utils::zh        =  90;
+int utils::zph        =  0;
+float utils::ylight  =   0;
+
+
+int main(int argc, char** argv) {
+    glutInit( & argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(1000, 1000);
+    glutCreateWindow("Kodur Krishna Chaitanya: HW5");
+    glPointSize(1);
+    glutDisplayFunc(utils::display_scene);
+    glutReshapeFunc(utils::reshape_window);
+    glutSpecialFunc(utils::special);
+    glutKeyboardFunc(utils::key);
+    glutIdleFunc(utils::idle);
+    glutMainLoop();
+    return 0;
+}
---
+++ homework_6/util.h
@@ -0,0 +1,46 @@
+#ifndef OPENGL_ASSIGNMENTS_LORENZ_H
+#define OPENGL_ASSIGNMENTS_LORENZ_H
+
+class utils {
+public:
+    /*
+     * Reference: ex13 from Moodle
+     * */
+    static int axes;
+    static int mode;
+    static int move;
+    static int th;
+    static int ph;
+    static int fov;
+    static int light;
+    static double asp;
+    static double dim;
+    static int one;
+    static int distance;
+    static int inc ;
+    static int smooth;
+    static int local ;
+    static int emission ;
+    static int ambient ;
+    static int diffuse ;
+    static int specular;
+    static int shininess;
+    static float shiny;
+    static int zh;
+    static int zph;
+    static float ylight;
+
+    static void display_scene();
+    static void reshape_window(int w, int h);
+    static void Vertex(double th,double ph);
+    static void ball(double x,double y,double z,double r);
+    static void idle();
+    static void draw_light_path();
+    static void key(unsigned char ch,int x,int y);
+    static void special(int key,int x,int y);
+    static float* get_me_normals(float* pt1, float* pt2, float *pt3);
+    static void draw_house(float translation);
+    static void draw_cone();
+
+};
+#endif //OPENGL_ASSIGNMENTS_LORENZ_H
